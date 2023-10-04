f0155894d7dce59894247da9621c1db16093df49
saad154 <saadahmed0021@gmail.com>
2023-10-04 14:22:53 +0500

    Method

--- Calculator1/Program.cs
+++ Calculator1/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
-namespace Calculator1
+namespace Methods
 {
     internal class Program
     {
         private static int Result;
+
         public static void Add(int a, int b)
         {
             Result = a + b;
@@ -25,18 +26,88 @@ namespace Calculator1
             Result = a / b;
         }
 
+        public static void Pass(int a)
+        {
+            if (a > 30)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+
+        public static void AGrade(int a)
+        {
+            if (a >= 90)
+            {
+                Console.WriteLine("A+ Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public static void BGrade(int a)
+        {
+            if (a >= 70)
+            {
+                Console.WriteLine("B Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        public static void CGrade(int a)
+        {
+            if (a >= 60)
+            {
+                Console.WriteLine("C Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        public static void DGrade(int a)
+        {
+            if (a >= 50)
+            {
+                Console.WriteLine("D Grade");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        private static void Fail(int a)
+            {
+                if (a <= 49)
+                {
+                    Console.WriteLine("F Grade");
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+
         public static void Main(string[] args)
         {
-            Add(5, 3);
-            Console.WriteLine("Answer is: " + Result);
-            Subtract(10, 45);
-            Console.WriteLine("Answer is: " + Result);
-            Multiply(9, 32);
-            Console.WriteLine("Answer is: " + Result);
-            Divide(36,6);
-            Console.WriteLine("Answer is: " + Result);
-            
-
+            Pass(90);
+            AGrade(90);
+            Pass(75);
+            BGrade(75);
+            Pass(64);
+            CGrade(64);
+            Pass(51);
+            DGrade(51);
+            Pass(45);
+            Fail(45);
         }
     }
 }
