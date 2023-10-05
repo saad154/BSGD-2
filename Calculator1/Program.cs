37a60bb935f3b7fe783cd74e0cc0388a83311c90
saad154 <67940805+saad154@users.noreply.github.com>
2023-10-05 21:45:05 +0500

    VAriable

--- Calculator1/Program.cs
+++ Calculator1/Program.cs
@@ -1,113 +1,110 @@
 ﻿using System;
 
-namespace Methods
+namespace CricketPlayers
 {
     internal class Program
     {
-        private static int Result;
-
-        public static void Add(int a, int b)
-        {
-            Result = a + b;
-        }
-
-        public static void Subtract(int a, int b)
-        {
-            Result = a - b;
-        }
-
-        public static void Multiply(int a, int b)
-        {
-            Result = a * b;
-        }
-
-        public static void Divide(int a, int b)
-        {
-            Result = a / b;
-        }
-
-        public static void Pass(int a)
-        {
-            if (a > 30)
-            {
-                Console.WriteLine("Pass");
-            }
-            else
-            {
-                Console.WriteLine("Fail");
-            }
-        }
-
-        public static void AGrade(int a)
-        {
-            if (a >= 90)
-            {
-                Console.WriteLine("A+ Grade");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-            }
-        }
-        public static void BGrade(int a)
-        {
-            if (a >= 70)
-            {
-                Console.WriteLine("B Grade");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-            }
-        }
-        public static void CGrade(int a)
-        {
-            if (a >= 60)
-            {
-                Console.WriteLine("C Grade");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-            }
-        }
-
-        public static void DGrade(int a)
-        {
-            if (a >= 50)
-            {
-                Console.WriteLine("D Grade");
-            }
-            else
-            {
-                Console.WriteLine("Error");
-            }
-        }
-
-        private static void Fail(int a)
-            {
-                if (a <= 49)
-                {
-                    Console.WriteLine("F Grade");
-                }
-                else
-                {
-                    Console.WriteLine("Error");
-                }
-            }
-
         public static void Main(string[] args)
         {
-            Pass(90);
-            AGrade(90);
-            Pass(75);
-            BGrade(75);
-            Pass(64);
-            CGrade(64);
-            Pass(51);
-            DGrade(51);
-            Pass(45);
-            Fail(45);
+            // Player 1
+            string name1 = "Babar Azam";
+            int age1 = 28;
+            float strikeRate1 = 89.1F;
+
+            Console.WriteLine("Player 1:");
+            Console.WriteLine("Name: " + name1);
+            Console.WriteLine("Age: " + age1);
+            Console.WriteLine("Strike Rate: " + strikeRate1);
+
+            // Player 2
+            string name2 = "Imam Ul Haq";
+            int age2 = 27;
+            float strikeRate2 = 82.18F;
+
+            Console.WriteLine("\nPlayer 2:");
+            Console.WriteLine("Name: " + name2);
+            Console.WriteLine("Age: " + age2);
+            Console.WriteLine("Strike Rate: " + strikeRate2);
+
+            // Player 3
+            string name3 = "Fakhar Zaman";
+            int age3 = 33;
+            float strikeRate3 = 91.96F;
+
+            Console.WriteLine("\nPlayer 3:");
+            Console.WriteLine("Name: " + name3);
+            Console.WriteLine("Age: " + age3);
+            Console.WriteLine("Strike Rate: " + strikeRate3);
+
+            // Player 4
+            string name4 = "Muhammad Rizwan";
+            int age4 = 31;
+            float strikeRate4 = 88.59F;
+
+            Console.WriteLine("\nPlayer 4:");
+            Console.WriteLine("Name: " + name4);
+            Console.WriteLine("Age: " + age4);
+            Console.WriteLine("Strike Rate: " + strikeRate4);
+
+            // Player 5
+            string name5 = "Haris Rauf";
+            int age5 = 29;
+            float strikeRate5 = 33.33F;
+
+            Console.WriteLine("\nPlayer 5:");
+            Console.WriteLine("Name: " + name5);
+            Console.WriteLine("Age: " + age5);
+            Console.WriteLine("Strike Rate: " + strikeRate5);
+
+            // Player 6
+            string name6 = "Naseem Shah";
+            int age6 = 20;
+            float strikeRate6 = 21.7F;
+
+            Console.WriteLine("\nPlayer 6:");
+            Console.WriteLine("Name: " + name6);
+            Console.WriteLine("Age: " + age6);
+            Console.WriteLine("Strike Rate: " + strikeRate6);
+
+            // Player 7
+            string name7 = "Shaheen Shah Afridi";
+            int age7 = 23;
+            float strikeRate7 = 78.77F;
+
+            Console.WriteLine("\nPlayer 7:");
+            Console.WriteLine("Name: " + name7);
+            Console.WriteLine("Age: " + age7);
+            Console.WriteLine("Strike Rate: " + strikeRate7);
+
+            // Player 8
+            string name8 = "Salman Ali Agha";
+            int age8 = 29;
+            float strikeRate8 = 96.46F;
+
+            Console.WriteLine("\nPlayer 8:");
+            Console.WriteLine("Name: " + name8);
+            Console.WriteLine("Age: " + age8);
+            Console.WriteLine("Strike Rate: " + strikeRate8);
+
+            // Player 9
+            string name9 = "Muhammad Nawaz";
+            int age9 = 29;
+            float strikeRate9 = 90.02F;
+
+            Console.WriteLine("\nPlayer 9:");
+            Console.WriteLine("Name: " + name9);
+            Console.WriteLine("Age: " + age9);
+            Console.WriteLine("Strike Rate: " + strikeRate9);
+
+            // Player 10
+            string name10 = "Shadab Khan";
+            int age10 = 25;
+            float strikeRate10 = 83.63F;
+
+            Console.WriteLine("\nPlayer 10:");
+            Console.WriteLine("Name: " + name10);
+            Console.WriteLine("Age: " + age10);
+            Console.WriteLine("Strike Rate: " + strikeRate10);
         }
     }
 }
